fd81b2797f51ddb5290387724ca392523fc93e9c
Leontiyar <vthehang@gmail.com>
2021-07-22 16:16:02 +0300

    Добавил бесконечный цикл, что бы не перезапускать калькулятор для повторного использования

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -6,155 +6,160 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет! Это калькулятор 2 чисел");
-            Console.WriteLine("Введи операцию (+,-,*,/,%)");
-            string operation = Console.ReadLine();
-            switch (operation)
+            while (true)
             {
-                case "+":
-                    {
-                        Console.WriteLine("Отлично! Сегодня будем суммировать? Теперь введи 1 число");
-                        bool smart = double.TryParse(Console.ReadLine(), out double a);
-                        if (!smart)//Переменная smart отвечает за то, умен ли юзер. Тоесть ввел ли он нормальное 1 число или бурду
+                Console.Clear();
+                Console.WriteLine("Привет! Это калькулятор 2 чисел");
+                Console.WriteLine("Введи операцию (+,-,*,/,%)");
+                string operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "+":
                         {
-                            Console.WriteLine("А начиналось так красиво...");
-                            Console.WriteLine("Я думал ты не дурачок");
-                            Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Замечательно! Жду 2 число");
-                            bool genius = double.TryParse(Console.ReadLine(), out double b);
-                            if (!genius)//Так же работает и переменная genius, только для 2 числа
+                            Console.WriteLine("Отлично! Сегодня будем суммировать? Теперь введи 1 число");
+                            bool smart = double.TryParse(Console.ReadLine(), out double a);
+                            if (!smart)//Переменная smart отвечает за то, умен ли юзер. Тоесть ввел ли он нормальное 1 число или бурду
                             {
-                                Console.WriteLine("Мужик, ну ты чего?");
+                                Console.WriteLine("А начиналось так красиво...");
                                 Console.WriteLine("Я думал ты не дурачок");
-                                Console.WriteLine("Так глупо проиграть в самом конце!");
                                 Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
                             }
                             else
                             {
-                                Console.WriteLine("Красава! Вот твоя сумма - " + (a + b));
+                                Console.WriteLine("Замечательно! Жду 2 число");
+                                bool genius = double.TryParse(Console.ReadLine(), out double b);
+                                if (!genius)//Так же работает и переменная genius, только для 2 числа
+                                {
+                                    Console.WriteLine("Мужик, ну ты чего?");
+                                    Console.WriteLine("Я думал ты не дурачок");
+                                    Console.WriteLine("Так глупо проиграть в самом конце!");
+                                    Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Красава! Вот твоя сумма - " + (a + b));
+                                }
                             }
                         }
-                    }
-                    break;
-                case "-":
-                    {
-                        Console.WriteLine("Отлично! Сегодня будем вычитать? Теперь введи 1 число");
-                        bool smart = double.TryParse(Console.ReadLine(), out double a);
-                        if (!smart)
+                        break;
+                    case "-":
                         {
-                            Console.WriteLine("А начиналось так красиво...");
-                            Console.WriteLine("Я думал ты не дурачок");
-                            Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Замечательно! Жду 2 число");
-                            bool genius = double.TryParse(Console.ReadLine(), out double b);
-                            if (!genius)
+                            Console.WriteLine("Отлично! Сегодня будем вычитать? Теперь введи 1 число");
+                            bool smart = double.TryParse(Console.ReadLine(), out double a);
+                            if (!smart)
                             {
-                                Console.WriteLine("Мужик, ну ты чего?");
+                                Console.WriteLine("А начиналось так красиво...");
                                 Console.WriteLine("Я думал ты не дурачок");
-                                Console.WriteLine("Так глупо проиграть в самом конце!");
                                 Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
                             }
                             else
                             {
-                                Console.WriteLine("Красава! Вот твоя разница - " + (a - b));
+                                Console.WriteLine("Замечательно! Жду 2 число");
+                                bool genius = double.TryParse(Console.ReadLine(), out double b);
+                                if (!genius)
+                                {
+                                    Console.WriteLine("Мужик, ну ты чего?");
+                                    Console.WriteLine("Я думал ты не дурачок");
+                                    Console.WriteLine("Так глупо проиграть в самом конце!");
+                                    Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Красава! Вот твоя разница - " + (a - b));
+                                }
                             }
                         }
-                    }
-                    break;
-                case "/":
-                    {
-                        Console.WriteLine("Отлично! Сегодня будем делить? Теперь введи 1 число");
-                        bool smart = double.TryParse(Console.ReadLine(), out double a);
-                        if (!smart)
+                        break;
+                    case "/":
                         {
-                            Console.WriteLine("А начиналось так красиво...");
-                            Console.WriteLine("Я думал ты не дурачок");
-                            Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Замечательно! Жду 2 число");
-                            bool genius = double.TryParse(Console.ReadLine(), out double b);
-                            if (!genius)
+                            Console.WriteLine("Отлично! Сегодня будем делить? Теперь введи 1 число");
+                            bool smart = double.TryParse(Console.ReadLine(), out double a);
+                            if (!smart)
                             {
-                                Console.WriteLine("Мужик, ну ты чего?");
+                                Console.WriteLine("А начиналось так красиво...");
                                 Console.WriteLine("Я думал ты не дурачок");
-                                Console.WriteLine("Так глупо проиграть в самом конце!");
                                 Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
                             }
                             else
                             {
-                                Console.WriteLine("Красава! Вот результат деления - " + (a / b));
+                                Console.WriteLine("Замечательно! Жду 2 число");
+                                bool genius = double.TryParse(Console.ReadLine(), out double b);
+                                if (!genius)
+                                {
+                                    Console.WriteLine("Мужик, ну ты чего?");
+                                    Console.WriteLine("Я думал ты не дурачок");
+                                    Console.WriteLine("Так глупо проиграть в самом конце!");
+                                    Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Красава! Вот результат деления - " + (a / b));
+                                }
                             }
                         }
-                    }
-                    break;
-                case "*":
-                    {
-                        Console.WriteLine("Отлично! Сегодня будем умножать? Теперь введи 1 число");
-                        bool smart = double.TryParse(Console.ReadLine(), out double a);
-                        if (!smart)
+                        break;
+                    case "*":
                         {
-                            Console.WriteLine("А начиналось так красиво...");
-                            Console.WriteLine("Я думал ты не дурачок");
-                            Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Замечательно! Жду 2 число");
-                            bool genius = double.TryParse(Console.ReadLine(), out double b);
-                            if (!genius)
+                            Console.WriteLine("Отлично! Сегодня будем умножать? Теперь введи 1 число");
+                            bool smart = double.TryParse(Console.ReadLine(), out double a);
+                            if (!smart)
                             {
-                                Console.WriteLine("Мужик, ну ты чего?");
+                                Console.WriteLine("А начиналось так красиво...");
                                 Console.WriteLine("Я думал ты не дурачок");
-                                Console.WriteLine("Так глупо проиграть в самом конце!");
                                 Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
                             }
                             else
                             {
-                                Console.WriteLine("Красава! Вот результат умножения - " + (a * b));
+                                Console.WriteLine("Замечательно! Жду 2 число");
+                                bool genius = double.TryParse(Console.ReadLine(), out double b);
+                                if (!genius)
+                                {
+                                    Console.WriteLine("Мужик, ну ты чего?");
+                                    Console.WriteLine("Я думал ты не дурачок");
+                                    Console.WriteLine("Так глупо проиграть в самом конце!");
+                                    Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Красава! Вот результат умножения - " + (a * b));
+                                }
                             }
                         }
-                    }
-                    break;
-                case "%":
-                    {
-                        Console.WriteLine("Отлично! Сегодня будем находить остаток от деления? Теперь введи 1 число");
-                        Console.WriteLine("С этой операцией доступны только натуральные числа!");//Вряд ли кому то нужны дробные числа при делении на остаток
-                        bool smart = int.TryParse(Console.ReadLine(), out int a);
-                        if (!smart)
+                        break;
+                    case "%":
                         {
-                            Console.WriteLine("А начиналось так красиво...");
-                            Console.WriteLine("Я думал ты не дурачок");
-                            Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Замечательно! Жду 2 число");
-                            bool genius = int.TryParse(Console.ReadLine(), out int b);
-                            if (!genius)
+                            Console.WriteLine("Отлично! Сегодня будем находить остаток от деления? Теперь введи 1 число");
+                            Console.WriteLine("С этой операцией доступны только натуральные числа!");//Вряд ли кому то нужны дробные числа при делении на остаток
+                            bool smart = int.TryParse(Console.ReadLine(), out int a);
+                            if (!smart)
                             {
-                                Console.WriteLine("Мужик, ну ты чего?");
+                                Console.WriteLine("А начиналось так красиво...");
                                 Console.WriteLine("Я думал ты не дурачок");
-                                Console.WriteLine("Так глупо проиграть в самом конце!");
                                 Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
                             }
                             else
                             {
-                                Console.WriteLine("Красава! Вот твой остаток от деления - " + (a % b));
+                                Console.WriteLine("Замечательно! Жду 2 число");
+                                bool genius = int.TryParse(Console.ReadLine(), out int b);
+                                if (!genius)
+                                {
+                                    Console.WriteLine("Мужик, ну ты чего?");
+                                    Console.WriteLine("Я думал ты не дурачок");
+                                    Console.WriteLine("Так глупо проиграть в самом конце!");
+                                    Console.WriteLine("Ну, впрочем, ладно. Попробуй еще раз, но по нормальному");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Красава! Вот твой остаток от деления - " + (a % b));
+                                }
                             }
                         }
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Дурачок, вводи доступные операции");
-                    break;
+                        break;
+                    default:
+                        Console.WriteLine("Дурачок, вводи доступные операции");
+                        break;
+                }
+                Console.ReadLine();
             }
         }
     }
